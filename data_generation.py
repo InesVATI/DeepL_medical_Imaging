7cd006d4c8987ef1b1bd1fcd6d5ddad7a4df7372
InesVATI <ines.vati@eleves.enpc.fr>
2022-03-16 22:49:05 +0100

    update create conductivity map

--- data_generation.py
+++ data_generation.py
@@ -13,85 +13,123 @@ from pyeit.eit.fem import Forward
 from pyeit.mesh.shape import thorax
 import matplotlib.pyplot as plt
 
-# reset the plot configurations to default
-#plt.rcdefaults()
-# set the axes color glbally for all plots
-#plt.rcParams.update({'axes.facecolor':'black'})
+import random as rd
 
-nb_px = 128
-
-def label_data(nb, ray=0.1, perm=1000.0):
+  
+def isintriangle(p1, p2, p3, p):
     """
-    Labelization of data 
-    --------------------
-    Parameters: 
-        nb: int, numerotation of the generated data, it is used in the file names 
-        ray: float, the ray of the simulated anomaly 
+    Enable to know either the point is inside the triangle defined by the vertices
+    by computing the Barycentric coordinates 
+    The method is described in : https://en.wikipedia.org/wiki/Barycentric_coordinate_system#Determining_location_with_respect_to_a_triangle 
+    
+    Params
+    -------
+    p: point array 2D shape
+    p1, p2, p3: vertices of the triangle numerotated in anticlockwise order
+    
+    Returns
+    -------
+    bool
         
     """
+    
+    detT = (p2[1]-p3[1])*(p1[0]-p3[0]) + (p3[0]-p2[0])*(p1[1]-p3[1])
+    c1 = ((p2[1]-p3[1])*(p[0]-p3[0]) + (p3[0]-p2[0])*(p[1]-p3[1]))/detT
+    if c1 < 0:
+        return False
+    
+    c2 = ((p3[1]- p1[1])*(p[0]-p3[0])+ (p1[0] -p3[0])*(p[1]-p3[1]))/detT
+    if c2 <0:
+        return False
+    
+    c3 = 1 - c1- c2
+    if c3<0:
+        return False
+    
+    return True
+          
+
+def label_data_rand(num, nb_px = 128, plot=False):
+    """"
+    Compute and save the conductivity map and the voltage at the borders
+    Params:
+    -----------
+    num: int used for the name of the file
+    nb_px: int 
+    
+    """
+    
+    #set the parameters of the random anomaly
+    ray = (0.7-0.04)*rd.random() + 0.04 #RAY randomly between 0.04 and 0.7 
+    print('ray is ', ray)
+    perm = (60000- 500)*rd.random() + 500
+    print('perm is ', perm)
+    #x in [-1, 1] , y in [-0.6, 0.6]
+    ano_x, ano_y = 2*rd.random() -1, 1.2*rd.random()- 0.6
+    
     #conductivity is equal to 1 every where
     mesh_obj, el_pos = mesh.create(16, h0=0.1, fd=thorax)
     # extract node, element, alpha
-    pts = mesh_obj["node"]  # Tableau de points qui constituent la discretisation du domaine
+    pts = mesh_obj["node"]  # array of coordinates (x,y) for each mesh's point 
     #For each triangle, the indices of the three points that make up the triangle, ordered in an anticlockwise manner.
-    tri = mesh_obj["element"] # Tableau de triangles. Un triangle = 1 tableau qui contient 3 indices qui identifient les points
+    tri = mesh_obj["element"]
     x, y = pts[:, 0], pts[:, 1]
     
-    #add almost-symmetrical anomaly
-    anomaly = [{"x": -0.25, "y": 0.0, "d": ray, "perm": perm}, {"x": 0.5, "y": 0.0, "d": ray, "perm": perm}]
+    #add one random anomaly 
+    anomaly = [{"x": ano_x, "y": ano_y, "d": ray, "perm": perm}]
     mesh_new = mesh.set_perm(mesh_obj, anomaly=anomaly)
     
-    #save figure
-    fig, ax = plt.subplots(1, constrained_layout=True)
-    fig.set_size_inches(6, 4)
-    
-    
     delta_perm = mesh_new["perm"] - mesh_obj["perm"] #match each triangle to a conductivity value
-    print("nb_pt: ", delta_perm.shape)
-    print("nb_x: ", x.shape)
-    print("nb_y: ", y.shape)
-    print("nb_triangle: ", tri.shape)
-
-    ax.tripcolor(x, y, tri, np.real(delta_perm), shading="flat")
-    img = np.zeros((nb_px, nb_px))
     
-    ax.set_aspect("equal")
-    # plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False) 
-    # plt.tick_params(axis='y', which='both', right=False, left=False, labelleft=False)
-    # ax.set_facecolor("black")
-    # plt.savefig('labeled_data/'+str(nb)+'_conductivity_img.jpg')
-    # plt.close() #prevent the figure from being plotted
-    plt.show()
+    """ 1. Save the array containing the conductivity """
+    cond_img = np.zeros((nb_px, nb_px))
+    
+    #graduation for pixelization of the image
+    xmin, xmax = np.min(x), np.max(x)
+    ymin, ymax = np.min(y), np.max(y)
+    grad_x = (xmax-xmin)/nb_px
+    grad_y = (ymax-ymin)/nb_px
+    
+    for i in range(nb_px):
+        for j in range (nb_px):
+            
+            # the pixel take the value of the conductivity of the triangle 
+            #in which its center is 
+            center = [(2*xmin + (2*i +1)*grad_x)/2, (2*ymin + (2*j +1)*grad_y)/2]
+            for k in range (tri.shape[0]):
+                p1 = [x[tri[k, 0]], y[tri[k,0]]]
+                p2 = [x[tri[k, 1]], y[tri[k,1]]]
+                p3 = [x[tri[k, 2]], y[tri[k,2]]]
+                
+                if isintriangle(p1, p2, p3, center):
+                    cond_img[i,j] = delta_perm[k]
+                    break 
+
+    if plot:
+        fig, ax = plt.subplots(1, constrained_layout=True)
+        fig.set_size_inches(6, 4)
+        
+        ax.tripcolor(x, y, tri, np.real(delta_perm), shading="flat")
+        ax.set_aspect("equal")
+        plt.show()
+        
+    #return cond_img
+    np.save('labeled_data/'+str(num)+'_cond_img', cond_img)
+    
     """ 2. FEM forward simulations """
     # setup EIT scan conditions
     # adjacent stimulation (el_dist=1), adjacent measures (step=1)
-    # el_dist, step = 1, 1
-    # ex_mat = eit_scan_lines(16, el_dist)
+    el_dist, step = 1, 1
+    ex_mat = eit_scan_lines(16, el_dist)
     
-    # # calculate simulated data
-    # fwd = Forward(mesh_obj, el_pos)
-    # f0 = fwd.solve_eit(ex_mat, step=step, perm=mesh_obj["perm"])
-    # f1 = fwd.solve_eit(ex_mat, step=step, perm=mesh_new["perm"])
-    # #with open("my_file", "w") as f:
+    # calculate simulated data
+    fwd = Forward(mesh_obj, el_pos)
+    f0 = fwd.solve_eit(ex_mat, step=step, perm=mesh_obj["perm"])
+    f1 = fwd.solve_eit(ex_mat, step=step, perm=mesh_new["perm"])
 
-    # np.save('labeled_data/'+str(nb)+'_voltage_border',f1.v - f0.v)
-
-#Test
-#V= np.load('labeled_data/0_voltage_border.npy')
-#print('TEST: ', V.shape)
-
-#loop
-def generation():
-    
-    ray = np.arange(0.015, 0.5, 0.005)
-    perm = np.linspace(10, 50000, 1000)
-    nb=1
-    for r in ray:
-        for p in perm:
-            label_data(nb, ray=r, perm=p)
-            nb += 1
-            
-    print('number of generated data: ', nb+1)
+    np.save('labeled_data/'+str(num)+'_voltage_border',f1.v - f0.v)
     
 
 
+def test(num=0):
+    label_data_rand(num)
